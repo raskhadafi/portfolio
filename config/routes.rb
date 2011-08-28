c81038ec9aedcb427db59091fe53946cf404956c
Roman Simecek <roman.simecek@cyt.ch>
2011-08-28 18:00:03 +0200

    Root url set to home#index.

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,8 @@ Portfolio::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => "home#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
